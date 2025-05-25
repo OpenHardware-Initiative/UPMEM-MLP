17539c36d7f9a33fb733537dcfb095c57a39ccb3
Kagan Dikmen <kagandikmen@outlook.com>
2025-05-25 12:19:36 +0200

    implement tiling among DPUs

--- Makefile
+++ Makefile
@@ -1,14 +1,16 @@
 CLANG = dpu-upmem-dpurte-clang
 SOURCE = matmul
-CFLAGS += -O0 -DNR_TASKLETS=16
-FILESTODELETE = matmul.c
+CFLAGS += -O0 -DNR_TASKLETS=6
+FILESTODELETE = matmul.c dpu/
 
 all:
 	python3 generate.py && \
-	${CLANG} ${CFLAGS} -o ${SOURCE}.o ${SOURCE}.c
+	for test in $$(seq 0 15); do \
+		$(CLANG) $(CFLAGS) -o dpu/dpu$$test/${SOURCE}.o dpu/dpu$$test/${SOURCE}.c; \
+	done
 
 clean:
 	rm -rf *.o ${FILESTODELETE}
 
 clean_all:
-	rm -rf *.o .vscode/ .cache/ .__pycache__/
+	rm -rf *.o .vscode/ .cache/ .__pycache__/ 
--- generate.py
+++ generate.py
@@ -1,10 +1,15 @@
+import os
 import random
 
-matrixa_dims = (32, 32)
-matrixb_dims = (32, 32)
+matrixa_dims = (96, 96)
+matrixb_dims = (96, 96)
 
-template_name = 'matmul.template'
-targetfile_name = 'matmul.c'
+num_dpu_used = 16
+
+template_name   = 'matrices.template'
+target_dir      = 'dpu/dpu{dpu}'
+targetc_name    = 'matmul.c'
+targeth_name    = 'matrices.h'
 
 def generateRandomMatrix(row, col):
     mat = []
@@ -15,23 +20,35 @@ def generateRandomMatrix(row, col):
 
 def main():
 
-    with open(template_name, mode='r') as tempfile:
-        template = tempfile.read()
+    for row in range(num_dpu_used):
+        with open(template_name, mode='r') as tempfile:
+            template = tempfile.read()
 
-    if(matrixa_dims[1] != matrixb_dims[0]):
-        raise ValueError('First matrix\' row count has to be equal to second matrix\' column count.')
+        if(matrixa_dims[1] != matrixb_dims[0]):
+            raise ValueError('First matrix\' row count has to be equal to second matrix\' column count.')
+        
+        tempfilled = template.format(
+                                    matrixa_rowcount=matrixa_dims[0]//num_dpu_used,
+                                    matrixa_colcount=matrixa_dims[1],
+                                    matrixb_rowcount=matrixb_dims[0],
+                                    matrixb_colcount=matrixb_dims[1],
+                                    dpu_nr = row,
+                                    mat1=', '.join(map(str, generateRandomMatrix(matrixa_dims[0]//num_dpu_used, matrixa_dims[1]))),
+                                    mat2=', '.join(map(str, generateRandomMatrix(matrixa_dims[0], matrixa_dims[1])))
+        )
+
+        target_dpu_dir = target_dir.format(dpu=row)
+
+        os.makedirs(target_dpu_dir, exist_ok=True)
+
+        with open(f'{target_dpu_dir}/{targeth_name}', mode='w') as cfile:
+            cfile.write(tempfilled)
     
-    tempfilled = template.format(
-                                matrixa_rowcount=matrixa_dims[0],
-                                matrixa_colcount=matrixa_dims[1],
-                                matrixb_rowcount=matrixb_dims[0],
-                                matrixb_colcount=matrixb_dims[1],
-                                mat1=', '.join(map(str, generateRandomMatrix(matrixa_dims[0], matrixa_dims[1]))),
-                                mat2=', '.join(map(str, generateRandomMatrix(matrixa_dims[0], matrixa_dims[1])))
-    )
+        with open('matmul.template', mode='r') as matmulfile:
+            template = matmulfile.read()
 
-    with open(targetfile_name, mode='w') as cfile:
-        cfile.write(tempfilled)
+        with open(f'{target_dpu_dir}/{targetc_name}', mode='w') as matmulfile:
+            matmulfile.write(template)
 
 
 if __name__ == '__main__':
