360d42bba161864c474e81a211b0de6537606ad8
Kagan Dikmen <kagandikmen@outlook.com>
2025-06-11 21:43:34 +0200

    include unit tests for read_image_data function

---
+++ tests/test_read_image_data.c
@@ -0,0 +1,21 @@
+#include "../read_image_data.c"
+
+int test_read_image_data()
+{
+    int ROWS;
+    int COLS = 5;
+    uint8_t **data = read_image_data("./test_read_image_data.txt", &ROWS, COLS);
+    return (data[0][0] == 5) && (data[0][1] == 6) && (data[1][0] == 7) && (data[1][1] == 8);
+}
+
+int main()
+{
+    int test_pass_fail = test_read_image_data();
+
+    if(test_pass_fail == 0)
+        printf("FAIL\n");
+    else
+        printf("PASS\n");
+    
+    return 0;
+}
