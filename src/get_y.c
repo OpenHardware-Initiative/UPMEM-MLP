966371930d0cc228ac051efaf231f2f638b99974
Kagan Dikmen <kagandikmen@outlook.com>
2025-06-14 02:31:57 +0200

    Implement get_y to compute activated values

---
+++ src/get_y.c
@@ -0,0 +1,23 @@
+#include "mlp.h"
+
+double *get_y(NETWORK *n, int layer_nr, double *samples)
+{
+    LAYER *current_layer = n->l+layer_nr;
+    int is_current_layer_last_layer = (n->num_layers == layer_nr + 1);
+    int y_size = current_layer->num_neurons;
+
+    if(!is_current_layer_last_layer) // add bias node
+        y_size++;
+
+    double *z = get_z(n, layer_nr, samples);
+    double *y = (double *) malloc (sizeof(double)*y_size);
+
+    for(size_t i=0; i<y_size; ++i) {
+        if(is_current_layer_last_layer)
+            y[i] = activation(z[i]);
+        else
+            y[i] = i==0 ? 1 : activation(z[i-1]);
+    }
+
+    return y;
+}
---
+++ tests/test_get_y.c
@@ -0,0 +1,59 @@
+#include "mlp.h"
+
+int test_get_y()
+{
+    int num_neurons_per_layers[] = {3, 3};
+    double samples[] = {1, 1, 1, 1};
+
+    NETWORK *n = init_network(3, 2, num_neurons_per_layers);
+
+    n->l[0].n[0].w[0] = -1.0;
+    n->l[0].n[0].w[1] = 1.0;
+    n->l[0].n[0].w[2] = 1.0;
+    n->l[0].n[0].w[3] = 1.0;
+
+    n->l[0].n[1].w[0] = -1.0;
+    n->l[0].n[1].w[1] = 5.0;
+    n->l[0].n[1].w[2] = 1.0;
+    n->l[0].n[1].w[3] = 1.0;
+
+    n->l[0].n[2].w[0] = -1.0;
+    n->l[0].n[2].w[1] = 0.0;
+    n->l[0].n[2].w[2] = 0.0;
+    n->l[0].n[2].w[3] = 0.0;
+
+    double *y = get_y(n, 0, samples);
+    double *z = get_z(n, 0, samples);
+
+    // printf("y[0] == %.2lf\n", y[0]);
+    // printf("y[1] == %.2lf\n", y[1]);
+    // printf("y[2] == %.2lf\n", y[2]);
+
+    int test_pass_fail = (y[0] == 1)
+                        && (y[1] == activation(z[0]))
+                        && (y[2] == activation(z[1]));
+
+    y = get_y(n, 1, samples);
+    z = get_z(n, 1, samples);
+
+    test_pass_fail = test_pass_fail
+                    && (y[0] == activation(z[0]))
+                    && (y[1] == activation(z[1]))
+                    && (y[2] == activation(z[2]));
+
+    return test_pass_fail;
+}
+
+int main()
+{
+    int test_pass_fail = test_get_y();
+    
+    if(test_pass_fail == 0)
+    {
+        printf("FAIL\n");
+        return 1;
+    }
+        
+    printf("PASS\n");
+    return 0;
+}
