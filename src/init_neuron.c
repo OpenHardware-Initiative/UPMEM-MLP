e8a23a950f64e2827dac0666572ebf51b8cd8c9f
Kagan Dikmen <kagandikmen@outlook.com>
2025-10-19 04:11:36 +0200

    Shift neuron weight initialization: [0,1]->[-1,1]

--- src/init_neuron.c
+++ src/init_neuron.c
@@ -8,9 +8,12 @@ NEURON *init_neuron(int num_weights)
     n->w    = (double *) malloc (sizeof(double) * n->num_weights);
     n->lw   = (double *) malloc (sizeof(double) * n->num_weights);
 
+    double limit = 1.0/sqrt((double) num_weights);
+
     for(int i=0; i<num_weights; i++)
     {
-        n->w[i] = (double) rand() / (double) RAND_MAX;
+        double rand_unit = (double)rand() / (double)RAND_MAX;
+        n->w[i] = (rand_unit * 2.0 - 1.0) * limit;
         n->lw[i] = 0;
     }
 
