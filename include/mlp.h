6799234a520160cac3af22ea04604db2da70bee2
Kagan Dikmen <kagandikmen@outlook.com>
2025-11-15 13:05:25 +0100

    Order macros & headers in `mlp.h` alphabetically

--- include/mlp.h
+++ include/mlp.h
@@ -7,10 +7,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#ifndef LEARNING_RATE
-#define LEARNING_RATE 0.1
-#endif
-
 #ifndef BATCH_SIZE
 #define BATCH_SIZE 1
 #endif
@@ -19,12 +15,16 @@
 #define EPSILON 0.0000005
 #endif
 
-#ifndef MOMENTUM
-#define MOMENTUM 0.8
+#ifndef LEARNING_RATE
+#define LEARNING_RATE 0.01
 #endif
 
 #ifndef MAX_EPOCH
 #define MAX_EPOCH 30
+#endif
+
+#ifndef MOMENTUM
+#define MOMENTUM 0.8
 #endif
 
 extern unsigned int rseed;
@@ -46,29 +46,34 @@ typedef struct {
     LAYER *l;
 } NETWORK;
 
+void accumulate_layer_gradients(LAYER *l, int batch_size);
+void apply_gradients(NETWORK *n, int batch_size);
+double drand();
 double get_activation(double x);
 double get_activation_derivative(double x);
-double *get_delta(NETWORK *N, double* samples, double* ideal, int layer_index);
-double drand();
-NEURON *init_neuron(int num_weights);
-LAYER *init_layer(int num_neurons, int num_weights_per_neuron, int batch_size);
-NETWORK *init_network(int num_inputs, int num_layers, int *num_inputs_per_layer, int batch_size);
+double *get_delta(NETWORK *n, double *samples, double *ideal, int layer_index);
 double *get_total_loss(NETWORK *n, double **samples, double **ideal, int nsamples);
 double *get_y(NETWORK *n, int layer_index, double *sample);
 double *get_z(NETWORK *n, int layer_index, double *sample);
+LAYER *init_layer(int num_neurons, int num_weights_per_neuron, int batch_size);
+NETWORK *init_network(int num_inputs, int num_layers, int *num_inputs_per_layer, int batch_size);
+NEURON *init_neuron(int num_weights);
+void multiply_matrix(const double *A, const double *B, double *C, int rows_a, int cols_a, int cols_b);
 uint8_t **read_image_data(const char *filename, int *num_rows, const int num_cols);
 double sse(double *real, double *ideal, int length);
-void accumulate_layer_gradients(LAYER *l, int batch_size);
-void apply_gradients(NETWORK* n, int batch_size);
-void multiply_matrix(const double *A, const double *B, double *C, int rows_a, int cols_a, int cols_b);
 void transpose_matrix(const double *A, double *C, int rows, int cols);
 
+//
 // utility functions
+//
+
 void free_layer(LAYER *l);
 void free_network(NETWORK *n);
 void free_neuron(NEURON *n);
+
 void free_double_matrix(double **addr, int nrows);
 void free_uint8_matrix(uint8_t **addr, int nrows);
+
 void print_double_matrix(double **addr, int nrows, int ncols);
 void print_double_vector(double *addr, int nrows);
 
