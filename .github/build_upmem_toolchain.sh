c9d9af1b21c03f79c5b868d2bbcc66ce0328f151
Kagan Dikmen <kagandikmen@outlook.com>
2025-11-23 22:03:02 +0100

    Update CI for recent changes in upmem-sdk repo

--- .github/build_upmem_toolchain.sh
+++ .github/build_upmem_toolchain.sh
@@ -2,6 +2,6 @@
 
 cd /opt/
 git clone https://github.com/kagandikmen/upmem-sdk.git
-tar -xvf upmem-sdk/upmem-2024.2.0-Linux-x86_64.tar.gz
+tar -xvf upmem-sdk/2024.2.0/upmem-2024.2.0-Linux-x86_64.tar.gz
 mv upmem-2024.2.0-Linux-x86_64/ /usr/local/bin/
 rm -rf upmem-sdk/
